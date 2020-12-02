858deef1a1ad0b457817501ddf51f16923819e2e
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-02 14:22:44 +0000

    Feat: User can give a string and it is output

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,1 +1,6 @@
-
+describe("Calculator", function() {
+  it('testing sum of 1*1', function() {
+    newCalculator = new Calculator
+  expect(newCalculator.result("1 * 1")).toEqual(["1 * 1", 1])
+  })
+})
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,22 +1,7 @@
-function Player() {
-}
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
+class Calculator {
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
+  result(string) {
+    return [string, 1]
   }
 
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
+}
