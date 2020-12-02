cc38a259c205c18184f3c33d05d7be49f07c886e
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-12-02 14:47:54 +0000

    feat: user can give an addition sum and it returns correctly

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -3,4 +3,9 @@ describe("Calculator", function() {
     newCalculator = new Calculator
   expect(newCalculator.result("1 * 1")).toEqual(["1 * 1", 1])
   })
+
+  it('testing sum of 1+1', function() {
+    newCalculator = new Calculator
+  expect(newCalculator.result("1 + 1")).toEqual(["1 + 1", 2])
+  })
 })
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,17 @@
 class Calculator {
 
   result(string) {
-    return [string, 1]
+    let stringSeparation = string.split(" ")
+    return this.calculator(stringSeparation[0], stringSeparation[1], stringSeparation[2])
+  }
+
+  calculator(number1, mathsign, number2) {
+    if(mathsign === "*"){
+     return [`${number1} ${mathsign} ${number2}`, number1*number2]
+    }else if (mathsign === "+"){
+      let sum = parseInt(number1) + parseInt(number2)
+      return [`${number1} ${mathsign} ${number2}`, sum]
+    }
   }
 
 }
