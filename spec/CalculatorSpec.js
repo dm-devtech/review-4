6a5d060328168fba57b50676db4affc63886e918
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-01-13 11:08:24 +0000

    Added additional tests for multiplication

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -4,6 +4,16 @@ describe("Calculator", function() {
   expect(newCalculator.result("1 * 1")).toEqual(["1 * 1", 1])
   })
 
+  it('testing sum of 4*2', function() {
+    newCalculator = new Calculator
+  expect(newCalculator.result("4 * 2")).toEqual(["4 * 2", 8])
+  })
+
+  it('testing sum of 2000*4', function() {
+    newCalculator = new Calculator
+  expect(newCalculator.result("2000 * 4")).toEqual(["2000 * 4", 8000])
+  })
+
   it('testing sum of 1+1', function() {
     newCalculator = new Calculator
   expect(newCalculator.result("1 + 1")).toEqual(["1 + 1", 2])
